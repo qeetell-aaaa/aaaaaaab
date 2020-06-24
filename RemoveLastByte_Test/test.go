bcd21a8bd74069cdb12989ec7e7a734a4a640564
Ibrahim Oladipupo Qamardeen [Qeetell] <ibrahim@qeetell.red>
2020-06-24 14:13:15 +0100

    Progress 200624-1413

---
+++ RemoveLastByte_Test/test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	tome "github.com/qeetell-aaaa/aaaaaaab"
+)
+
+func main () {
+	some := []byte {1, 2, 3, 4, 5, 6, 7, 8}
+	tome := pkge.RemoveLastByte (some)
+	fmt.Println (some, tome)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qeetell-aaaa/aaaaaaab
+
+go 1.14
